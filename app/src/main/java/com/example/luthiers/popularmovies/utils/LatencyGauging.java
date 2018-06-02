3014035682c212acd82d281b829c1b183df288d9
Andrey Valverde Solera <andrey.valverde.solera@gmail.com>
2018-06-02 15:59:30 -0600

    add circular reveal animation for the fab revealing the filters layout

--- app/src/main/java/com/example/luthiers/popularmovies/utils/LatencyGauging.java
+++ app/src/main/java/com/example/luthiers/popularmovies/utils/LatencyGauging.java
@@ -26,49 +26,55 @@ public class LatencyGauging {
             @SuppressLint("MissingPermission") //TODO add proper permission
                     NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
             
-            //Get the current activeNetwork type
-            switch (activeNetwork.getType()) {
-                //We could only set the Mobile network type case and the default case, but for better readability set the Wifi type case also
-                case (ConnectivityManager.TYPE_WIFI): {
-                    //Apply standard latency strategy
-                    return Constants.MEDIUM_RES_IMAGE_SIZE;
-                }
-                
-                //The mobile type case has various types of network
-                case (ConnectivityManager.TYPE_MOBILE): {
-                    /*
-                     * According to a post on LinkedIn => https://www.linkedin.com/pulse/differences-between-gprs-edge-3g-hsdpa-hspa-4g-lte-ransilu
-                     * We can set an explanation for the various types of mobile network
-                     * */
+            //Check if the user has an active network
+            if (activeNetwork != null) {
+                //Get the current activeNetwork type
+                switch (activeNetwork.getType()) {
+                    //We could only set the Mobile network type case and the default case, but for better readability set the Wifi type case also
+                    case (ConnectivityManager.TYPE_WIFI): {
+                        //Apply standard latency strategy
+                        return Constants.MEDIUM_RES_IMAGE_SIZE;
+                    }
                     
-                    //Check that the telephony manager is not null
-                    if (telephonyManager != null) {
-                        switch (telephonyManager.getNetworkType()) {
-                            case (TelephonyManager.NETWORK_TYPE_LTE): {
-                                //LTE is a 4G communication standard that supports HD video streaming, download speed as high as 299.6Mbps
-                                return Constants.HIGH_RES_IMAGE_SIZE;
+                    //The mobile type case has various types of network
+                    case (ConnectivityManager.TYPE_MOBILE): {
+                        /*
+                         * According to a post on LinkedIn => https://www.linkedin.com/pulse/differences-between-gprs-edge-3g-hsdpa-hspa-4g-lte-ransilu
+                         * We can set an explanation for the various types of mobile network
+                         * */
+                        
+                        //Check that the telephony manager is not null
+                        if (telephonyManager != null) {
+                            switch (telephonyManager.getNetworkType()) {
+                                case (TelephonyManager.NETWORK_TYPE_LTE): {
+                                    //LTE is a 4G communication standard that supports HD video streaming, download speed as high as 299.6Mbps
+                                    return Constants.HIGH_RES_IMAGE_SIZE;
+                                }
+                                
+                                case (TelephonyManager.NETWORK_TYPE_EDGE): {
+                                    //EDGE is significantly faster with a download speed of up to 384Kbps
+                                    return Constants.MEDIUM_RES_IMAGE_SIZE;
+                                }
+                                
+                                case (TelephonyManager.NETWORK_TYPE_GPRS): {
+                                    //GPRS is a packet-based* wireless communication service. It is a 2G technology network that support a download speed of up to 114Kbps.
+                                    //Apply lower latency strategy
+                                    return Constants.LOW_RES_IMAGE_SIZE;
+                                }
+                                
+                                default:
+                                    return Constants.MEDIUM_RES_IMAGE_SIZE;
                             }
-                            
-                            case (TelephonyManager.NETWORK_TYPE_EDGE): {
-                                //EDGE is significantly faster with a download speed of up to 384Kbps
-                                return Constants.MEDIUM_RES_IMAGE_SIZE;
-                            }
-                            
-                            case (TelephonyManager.NETWORK_TYPE_GPRS): {
-                                //GPRS is a packet-based* wireless communication service. It is a 2G technology network that support a download speed of up to 114Kbps.
-                                //Apply lower latency strategy
-                                return Constants.LOW_RES_IMAGE_SIZE;
-                            }
-                            
-                            default:
-                                return Constants.MEDIUM_RES_IMAGE_SIZE;
                         }
                     }
+                    
+                    default:
+                        return Constants.MEDIUM_RES_IMAGE_SIZE;
                 }
-                
-                default:
-                    return Constants.MEDIUM_RES_IMAGE_SIZE;
+            } else {
+                return Constants.MEDIUM_RES_IMAGE_SIZE;
             }
+            
         } else {
             //The connectivityManager instance was null, but return the MEDIUM_RES_IMAGE_SIZE
             return Constants.MEDIUM_RES_IMAGE_SIZE;
--- app/src/main/java/com/example/luthiers/popularmovies/views/MainActivity.java
+++ app/src/main/java/com/example/luthiers/popularmovies/views/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.luthiers.popularmovies.views;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.ActivityOptions;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,6 +14,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Toast;
 
 import com.example.luthiers.popularmovies.utils.Constants;
@@ -22,34 +28,45 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     
     private String mFilter;//Initialize the filter as most popular by default
     private MovieViewModel mMovieViewModel;
+    private FloatingActionButton mFab;
+    private ConstraintLayout mFiltersLayout;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //Initialize the filters layout
+        mFiltersLayout = findViewById(R.id.filters_layout);
         
-        findViewById(R.id.fab).setOnClickListener(v -> {
-            //...
+        //Initialize the fab
+        mFab = findViewById(R.id.fab);
+        //Set the click listener for the fab
+        mFab.setOnClickListener(v -> {
+            //Check if we need to start or exit the circular reveal animation
+            if (mFiltersLayout.getVisibility() == View.INVISIBLE) doCircularReveal();
+            else exitCircularReveal();
         });
         
         //Create an instance of the MoviesAdapter
         MoviesAdapter moviesAdapter = new MoviesAdapter(this);
         
+        //Set the layout manager to the recycler view, it's going to be the GridLayoutManager with 2 columns
+        int NUMBER_OF_COLUMNS = 2;
+        
+        //Set the filter to be most popular as default
+        mFilter = Constants.MOST_POPULAR;
+        
         //Initialize a recycler view
         RecyclerView recyclerView = findViewById(R.id.rv_movies);
         //The size of each item from the recycler view won't change, so set this value to be true, to let recycler view do some optimizations
         recyclerView.setHasFixedSize(true);
-        //Set the layout manager to the recycler view, it's going to be the GridLayoutManager with 2 columns
-        int NUMBER_OF_COLUMNS = 2;
         recyclerView.setLayoutManager(new GridLayoutManager(this, NUMBER_OF_COLUMNS));
         //Set the moviesAdapter to the recycler view
         recyclerView.setAdapter(moviesAdapter);
         
         //Get the movies from the ViewModel
         mMovieViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
-        //Set the filter to be most popular as default
-        mFilter = Constants.MOST_POPULAR;
         //Set the mFilter variable from the view model to be the current mFilter variable
         mMovieViewModel.mFilter.setValue(mFilter);
         //Listen to the movies gotten from the repository
@@ -58,6 +75,61 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
                 Toast.makeText(this, R.string.error_displaying_movies, Toast.LENGTH_LONG).show();
             else moviesAdapter.addList(movies);
         });
+    }
+    
+    private void doCircularReveal() {
+        //Get the center of the fab so that the circular reveal starts there
+        int centerX = (mFab.getLeft() + mFab.getRight()) / 2;
+        int centerY = mFab.getHeight() / 2;
+        
+        float startRadius = 0;
+        
+        //The final end radius should the the max value for the filters layout
+        float endRadius = (float) Math.hypot(mFiltersLayout.getWidth(), mFiltersLayout.getHeight());
+        
+        //Create the animator for the filter view
+        Animator animation = ViewAnimationUtils.createCircularReveal(mFiltersLayout, centerX, centerY, startRadius, endRadius);
+        animation.setDuration(150);
+        animation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                
+                //Remove the surrounding shadow of the fab
+                mFab.setElevation(0);
+            }
+        });
+        
+        //Set the filter layout to be invisible
+        mFiltersLayout.setVisibility(View.VISIBLE);
+        
+        animation.start();
+    }
+    
+    private void exitCircularReveal() {
+        //Get the center of the fab so that the circular reveal starts there
+        int centerX = (mFab.getLeft() + mFab.getRight()) / 2;
+        int centerY = mFab.getHeight() / 2;
+        
+        float initialRadius = mFiltersLayout.getWidth();
+        
+        Animator animation = ViewAnimationUtils.createCircularReveal(mFiltersLayout, centerX, centerY, initialRadius, 0);
+        animation.setDuration(150);
+        // make the view invisible when the animation is done
+        animation.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                //Set the visibility of the filters layout as View.INVISIBLE
+                mFiltersLayout.setVisibility(View.INVISIBLE);
+                
+                //Add the elevation to the fab again
+                mFab.setElevation(6); //6dp elevation according to the Material Design guidelines
+            }
+        });
+        
+        // start the animation
+        animation.start();
     }
     
     @Override
@@ -94,6 +166,11 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     
     @Override
     public void onMovieItemClicked(Movie movie, MoviesAdapter.MovieViewHolder holder) {
+        //Check if the filters layout is visible, if it is then exitCircularReveal
+        if (mFiltersLayout.getVisibility() == View.VISIBLE) {
+            exitCircularReveal();
+        }
+        
         //Set the proper intent to open the DetailActivity and send the movie pojo
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("movie", movie);
