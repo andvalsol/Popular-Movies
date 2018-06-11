9ac8959cea327f809dfd34b4a21263167f467d1d
Andrey Valverde Solera <andrey.valverde.solera@gmail.com>
2018-06-10 18:41:44 -0600

    Project ready for revision

--- app/src/main/java/com/example/luthiers/popularmovies/utils/Constants.java
+++ app/src/main/java/com/example/luthiers/popularmovies/utils/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
     public static final String MEDIUM_RES_IMAGE_SIZE = "w185";
     public static final String HIGH_RES_IMAGE_SIZE = "w500";
     
-    public static final String MOVIE_DB_API_KEY = "815a3956be465dbb86b8d9af4d3eb583";
+    public static final String MOVIE_DB_API_KEY = ""; //TODO add the proper API Key
     public static final String MOST_POPULAR = "/discover/movie?sort_by=popularity.desc";
     public static final String TOP_RATED = " /discover/movie/?certification_country=US&sort_by=vote_average.desc";
 
